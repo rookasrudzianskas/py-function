e1de67ed5421c6ffe9adef58c4e9640c96f2f8d3
rookasrudzianskas <rookas.rudzenskas@gmail.com>
2023-11-07 20:46:20 +0200

    add one lliner

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+import random
+
 a = ["Stunning", "Good", "Crazy"]
 n = ["Compoass", "Banana", "Geek"]
 
 def generate_band_name():
-    current_time = int((id(a) + id(n)) / 1000000)
-    adjective_index = current_time % len(a)
-    noun_index = (current_time // len(a)) % len(n)
-    
-    adjective = a[adjective_index]
-    noun = n[noun_index]
-    print(f"Generated: {adjective} {noun}")
+  print(f"Generated band name: {random.choice(a)} {random.choice(n)}")
 
 generate_band_name()
