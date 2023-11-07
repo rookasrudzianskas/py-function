1cdb677cec7a87508853bf0690c7b648b407510e
rookasrudzianskas <rookas.rudzenskas@gmail.com>
2023-11-07 21:08:37 +0200

    one liner at all

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ def generate_band_name():
   print(f"Generated band name: {random.choice(a)} {random.choice(n)}")
 
 generate_band_name()
+
+// Arba sita one lineri
+
+import random; print(f"Generated band name: {random.choice(['Stunning', 'Good', 'Crazy'])} {random.choice(['Compoass', 'Banana', 'Geek'])}")
